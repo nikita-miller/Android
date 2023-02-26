22ffe99b2033c911c4b6f1ec3467d0d9ce161781
Nikita Miller <melscre4m@gmail.com>
2023-02-26 15:37:15 +0200

    change chat api url

--- app/src/main/java/step/learning/ChatActivity.java
+++ app/src/main/java/step/learning/ChatActivity.java
@@ -1,7 +1,15 @@
 package step.learning;
 
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -11,8 +19,12 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -29,7 +41,7 @@ import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -37,6 +49,7 @@ import java.util.UUID;
 
 public class ChatActivity extends AppCompatActivity {
     private String chatUrlString;
+    private String channelId;
     private String content;
     private LinearLayout chatContainer;
     private List<ChatMessage> chatMessages;
@@ -44,6 +57,8 @@ public class ChatActivity extends AppCompatActivity {
     private EditText etUserMessage;
     private ChatMessage userMessage;
     private ScrollView svChatMessages;
+    private Handler handler;
+    private MediaPlayer incomingMessagePlayer;
 
     private static final SimpleDateFormat scanFormat =
             new SimpleDateFormat(
@@ -56,9 +71,8 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        new Thread(this::loadUrl).start();
-
         chatUrlString = getString(R.string.chat_api_url);
+        channelId = getString(R.string.channel_id);
 
         chatContainer = findViewById(R.id.chat_container);
         etUserName = findViewById(R.id.et_user_name);
@@ -67,11 +81,23 @@ public class ChatActivity extends AppCompatActivity {
 
         findViewById(R.id.btn_chat_send)
                 .setOnClickListener(this::sendButtonClick);
+
+        chatMessages = new ArrayList<>();
+        handler = new Handler();
+        handler.post(this::updateChat);
+
+        incomingMessagePlayer = MediaPlayer.create(this, R.raw.sound_1);
+//        handler.postDelayed(this::showNotification, 3000);
+    }
+
+    private void updateChat() {
+        new Thread(this::loadUrl).start();
+        handler.postDelayed(this::updateChat, 3000);
     }
 
     private void sendButtonClick(View v) {
         String author = etUserName.getText().toString();
-        if (author.isEmpty()) {
+        if (author.trim().isEmpty()) {
             Toast.makeText(
                     this,
                     "Enter author's name",
@@ -80,10 +106,23 @@ public class ChatActivity extends AppCompatActivity {
             etUserName.requestFocus();
             return;
         }
+
         String messageTxt = etUserMessage.getText().toString();
+        if (messageTxt.trim().isEmpty()) {
+            Toast.makeText(
+                    this,
+                    "Enter text of the message",
+                    Toast.LENGTH_SHORT
+            ).show();
+            etUserMessage.requestFocus();
+            return;
+        }
+        etUserMessage.setText("");
+
         this.userMessage = new ChatMessage();
         this.userMessage.setAuthor(author);
         this.userMessage.setTxt(messageTxt);
+
         new Thread(this::postUserMessage).start();
     }
 
@@ -118,7 +157,10 @@ public class ChatActivity extends AppCompatActivity {
             while ((length = response.read(chunk)) > -1) {
                 bytes.write(chunk, 0, length);
             }
-            String responseBody = new String(bytes.toByteArray(), StandardCharsets.UTF_8);
+            String responseBody = new String(
+                    bytes.toByteArray(),
+                    StandardCharsets.UTF_8
+            );
             Log.i("ChatActivity::postUserMessage", responseBody);
 
             bytes.close();
@@ -156,17 +198,21 @@ public class ChatActivity extends AppCompatActivity {
             JSONObject object = new JSONObject(content);
             if ("success".equals(object.getString("status"))) {
                 JSONArray array = object.getJSONArray("data");
-                chatMessages = new ArrayList<>();
-                StringBuilder sb = new StringBuilder();
+
                 int length = array.length();
                 for (int i = 0; i < length; ++i) {
-                    chatMessages.add(
-                            new ChatMessage(
-                                    array.getJSONObject(i)
-                            )
+                    ChatMessage tmp = new ChatMessage(
+                            array.getJSONObject(i)
                     );
+                    if (chatMessages.stream().noneMatch(
+                            cm -> cm.getId().equals(tmp.getId())
+                    )
+                    ) {
+                        chatMessages.add(tmp);
+                    }
                 }
-                Collections.reverse(chatMessages);
+                chatMessages.sort(Comparator.comparing(ChatMessage::getMoment));
+
                 runOnUiThread(this::showChatMessages);
             }
         } catch (JSONException ex) {
@@ -193,7 +239,6 @@ public class ChatActivity extends AppCompatActivity {
                         LinearLayout.LayoutParams.WRAP_CONTENT
                 );
         otherLayoutParams.setMargins(10, 7, 10, 7);
-
         LinearLayout.LayoutParams myLayoutParams =
                 new LinearLayout.LayoutParams(
                         LinearLayout.LayoutParams.WRAP_CONTENT,
@@ -205,7 +250,13 @@ public class ChatActivity extends AppCompatActivity {
         TextView tvMessage;
         boolean isMyMessage;
         String author;
+        boolean scrollNeeded = false;
         for (ChatMessage chatMessage : chatMessages) {
+            if (chatMessage.getView() != null) {
+                continue;
+            }
+
+            Log.d("uuid", chatMessage.getId().toString());
             tvMessage = new TextView(this);
             author = chatMessage.getAuthor();
             isMyMessage = author.equals(etUserName.getText().toString());
@@ -228,17 +279,73 @@ public class ChatActivity extends AppCompatActivity {
             tvMessage.setTextSize(16);
             tvMessage.setPadding(10, 5, 10, 5);
             chatContainer.addView(tvMessage);
+            chatMessage.setView(tvMessage);
+            tvMessage.setTag(chatMessage);
+            scrollNeeded = true;
         }
 
-        // после того как добавлены все TextView происходит скролл вниз
-        // метод post отправляет runnable object в очередь сообщений приложения
-        // и выполняет его в UI потоке
-        svChatMessages.post(
-                () -> svChatMessages.fullScroll(ScrollView.FOCUS_DOWN)
-        );
+        if (scrollNeeded) {
+            // после того как добавлены все TextView происходит скролл вниз
+            // метод post отправляет runnable object в очередь сообщений приложения
+            // и выполняет его в UI потоке
+            svChatMessages.post(
+                    () -> svChatMessages.fullScroll(ScrollView.FOCUS_DOWN)
+            );
+        }
+    }
+
+    private void showNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(channelId, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                        .setContentTitle("Chat")
+                        .setContentText("New message in chat")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        Notification notification = notificationBuilder.build();
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{Manifest.permission.POST_NOTIFICATIONS},
+                        10002
+                );
+
+                return;
+            }
+        }
+
+        notificationManager.notify(1002, notification);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String[] permissions,
+            @NonNull int[] grantResults
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 10002) {
+        }
     }
 
     private static class ChatMessage {
+        private View view;
         private UUID id;
         private String author;
         private String txt;
@@ -260,7 +367,7 @@ public class ChatActivity extends AppCompatActivity {
             setTxt(object.getString("txt"));
             String moment = object.getString("moment");
             try {
-                setMoment(scanFormat.parse(moment));
+                setMoment(scanFormat.parse("Feb 26, 2023 9:23:59 PM"));
             } catch (ParseException ex) {
                 throw new JSONException("Date (moment) parse error: " + moment);
             }
@@ -289,6 +396,14 @@ public class ChatActivity extends AppCompatActivity {
             sb.append("}");
 
             return sb.toString();
+        }
+
+        public View getView() {
+            return view;
+        }
+
+        public void setView(View view) {
+            this.view = view;
         }
 
         public UUID getId() {
